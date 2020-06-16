8330090e01f9e8d6c8d50a8d8eb6f9a83cce9a97
Someone <rgbjkbs@hdbf.ru>
2020-06-16 18:23:21 +0300

    init

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My site</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+	</body>
+</html>
